352759bb0a7a71eaaa99aec7fa0cd40d15463b08
ivanmalyar <ivanmalyar84@gmail.com>
2018-03-11 10:49:21 +0900

    2 commit

--- Calculate.java
+++ Calculate.java
@@ -1,5 +1,9 @@
 public class Calculate {
     public static void main(String[] args) {
-        System.out.println("Hi!");
+        int first = Integer.valueOf(args[0]);
+        int second = Integer.valueOf(args[1]);
+        System.out.println(first + second);
+
+
     }
 }
